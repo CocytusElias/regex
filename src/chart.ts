82b10ef90b75ff98d3659dea84f3fd0480f08b1e
CocytusElias <elias@elias.ink>
2022-03-09 17:06:20 +0800

    feat: 增加 chart 相关正则的测试用例，并修复出问题的正则表达式

--- src/chart.ts
+++ src/chart.ts
@@ -1,12 +1,12 @@
 export const binary = /^[01]+$/;
 
-export const hex = /^[a-fA-F\d]$/;
+export const hex = /^[a-fA-F\d]+$/;
 
 export const hexLower = /^[a-f\d]+$/;
 
 export const hexUpper = /^[A-F\d]+$/;
 
-export const hexColor = /^#?([a-fA-F\d]{6}|[a-fA-F\d]{3})$/;
+export const hexColor = /^#([a-fA-F\d]{6}|[a-fA-F\d]{3})$/;
 
 export const cnChart = /^[\u4e00-\u9fa5]+$/;
 
